28cb0ad9b96b4adfaee568dc5b6a58c1aef703ca
reema-89 <reemahabib99@gmail.com>
2025-03-29 11:01:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+//assignment operators
+let a=200;
+let b=20;
+a+=b;
+console.log(a);
+console.log(a==b);
+
+//comparison operators
+console.log(a==b);
+console.log(a!=b);
+console.log(a>b);
+console.log(a<b);
+console.log(a>=b);
+console.log(a<=b);
+console.log(a===b);
+
+//logical operators
+let x=true;
+let y=false;
+console.log(x && y);
+console.log(x||y);
+console.log(!x);
+
+//arithematic operators
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);
+
+
+
+
